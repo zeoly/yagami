1ee605d90cc709de80ea36c09790388e3b637c77
zeoly <zeoly100@163.com>
2017-07-05 14:34:44 +0800

    eureka HA

--- anochat-eureka/src/main/java/com/thinkequip/anochat/eureka/Application.java
+++ anochat-eureka/src/main/java/com/thinkequip/anochat/eureka/Application.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
-	
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 }
