fb9bc8bcaf9ea97e09bb72ce25f81da3ce3b0db5
zeoly <zeoly100@163.com>
2017-07-25 17:02:44 +0800

    add unit test

---
+++ yagami-all/yagami-base/src/test/java/com/yahacode/yagami/pd/DepartmentServiceTest.java
@@ -0,0 +1,111 @@
+package com.yahacode.yagami.pd;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yahacode.yagami.base.BaseTest;
+import com.yahacode.yagami.base.BizfwServiceException;
+import com.yahacode.yagami.base.consts.ErrorCode;
+import com.yahacode.yagami.pd.model.Department;
+import com.yahacode.yagami.pd.service.DepartmentService;
+
+public class DepartmentServiceTest extends BaseTest {
+
+	@Autowired
+	private DepartmentService departmentService;
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Test
+	public void testAddDepartment() throws BizfwServiceException {
+		Department department = new Department("test");
+		department.setCode("test");
+		department.setName("测试");
+		Department parentDepartment = departmentService.queryByCode("root");
+		department.setParentDepartmentId(parentDepartment.getIdBfDepartment());
+		departmentService.addDepartment(department);
+
+		Department dbDepartment = departmentService.queryByCode("test");
+		assertEquals("测试", dbDepartment.getName());
+	}
+
+	@Test
+	public void testModifyDepartment() throws BizfwServiceException {
+		Department department = departmentService.queryByCode("root");
+		department.setName("测试修改");
+		departmentService.modifyDepartment(department);
+
+		Department dbDepartment = departmentService.queryByCode("root");
+		assertEquals("测试修改", dbDepartment.getName());
+	}
+
+	@Test
+	public void testDeleteDepartmentHasChild() throws BizfwServiceException {
+		expectedException.expect(BizfwServiceException.class);
+		expectedException.expectMessage(ErrorCode.PeopleDept.Dept.DEL_FAIL_WITH_CHILD);
+
+		Department department = departmentService.queryByCode("root");
+		departmentService.deleteDepartment(department);
+	}
+
+	@Test
+	public void testDeleteDepartmentHasPeople() throws BizfwServiceException {
+		expectedException.expect(BizfwServiceException.class);
+		expectedException.expectMessage(ErrorCode.PeopleDept.Dept.DEL_FAIL_WITH_PEOPLE);
+
+		Department department = departmentService.queryByCode("11");
+		departmentService.deleteDepartment(department);
+	}
+
+	@Test
+	public void testDeleteDepartment() throws BizfwServiceException {
+		Department department = departmentService.queryByCode("211");
+		departmentService.deleteDepartment(department);
+
+		Department dbDepartment = departmentService.queryByCode("211");
+		assertNull(dbDepartment);
+	}
+
+	@Test
+	public void testGetParentDepartment() throws BizfwServiceException {
+		Department department = departmentService.queryByCode("1");
+		Department parentDepartment = departmentService.getParentDepartment(department);
+		assertEquals("root", parentDepartment.getName());
+	}
+
+	@Test
+	public void testGetChildDepartmentList() throws BizfwServiceException {
+		Department department = departmentService.queryByCode("root");
+		List<Department> list = departmentService.getChildDepartmentList(department.getIdBfDepartment());
+		assertEquals(list.size(), 3);
+	}
+
+	@Test
+	public void testGetAllParentDeptmentList() throws BizfwServiceException {
+		Department department = departmentService.queryByCode("1");
+		List<Department> list = departmentService.getAllParentDeptmentList(department.getIdBfDepartment());
+		assertEquals(list.size(), 1);
+	}
+
+	@Test
+	public void testGetAllChildDeptmentList() throws BizfwServiceException {
+		Department department = departmentService.queryByCode("1");
+		List<Department> list = departmentService.getAllChildDeptmentList(department.getIdBfDepartment());
+		assertEquals(list.size(), 3);
+	}
+
+	@Test
+	public void testGetDepartmentTreeByDepartmentId() throws BizfwServiceException {
+		Department department = departmentService.queryByCode("1");
+		Department node = departmentService.getDepartmentTreeByDepartmentId(department.getIdBfDepartment());
+		assertEquals(node.getChildDepartmentList().size(), 2);
+	}
+}
---
+++ yagami-all/yagami-base/src/test/java/com/yahacode/yagami/pd/PeopleServiceTest.java
@@ -0,0 +1,134 @@
+package com.yahacode.yagami.pd;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yahacode.yagami.base.BaseTest;
+import com.yahacode.yagami.base.BizfwServiceException;
+import com.yahacode.yagami.base.common.StringUtils;
+import com.yahacode.yagami.base.consts.ErrorCode;
+import com.yahacode.yagami.pd.model.People;
+import com.yahacode.yagami.pd.service.PeopleService;
+
+public class PeopleServiceTest extends BaseTest {
+
+	@Autowired
+	private PeopleService peopleService;
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Test
+	public void testAddPeopleExists() throws BizfwServiceException {
+		expectedException.expect(BizfwServiceException.class);
+		expectedException.expectMessage(ErrorCode.PeopleDept.People.ADD_FAIL_EXISTED);
+
+		People people = new People("test");
+		people.setCode("zengyongli");
+		people.setName("曾永理");
+		people.setDepartmentId("0");
+		peopleService.addPeople(people);
+	}
+
+	@Test
+	public void testAddPeopleDeptErr() throws BizfwServiceException {
+		expectedException.expect(BizfwServiceException.class);
+		expectedException.expectMessage(ErrorCode.PeopleDept.People.ADD_FAIL_WITHOUT_DEPT);
+
+		People people = new People("test");
+		people.setCode("testadd");
+		people.setName("testadd");
+		people.setDepartmentId("0");
+		peopleService.addPeople(people);
+	}
+
+	@Test
+	public void testAddPeople() throws BizfwServiceException {
+		People people = new People("test");
+		people.setCode("testadd");
+		people.setName("testadd");
+		people.setDepartmentId("8a808087583fa7b701583faadf300000");
+		String id = peopleService.addPeople(people);
+
+		People dbPeople = peopleService.queryById(id);
+		assertEquals(dbPeople.getCode(), "testadd");
+	}
+
+	@Test
+	public void testModifyPeople() throws BizfwServiceException {
+		People people = peopleService.getByCode("zengyongli");
+		people.setName("testmodify");
+		peopleService.modifyPeople(people);
+
+		People dbPeople = peopleService.getByCode("zengyongli");
+		assertEquals(dbPeople.getName(), "testmodify");
+	}
+
+	@Test
+	public void testDeletePeopleDelSelf() throws BizfwServiceException {
+		expectedException.expect(BizfwServiceException.class);
+		expectedException.expectMessage(ErrorCode.PeopleDept.People.DEL_FAIL_SELF);
+
+		People people = peopleService.getByCode("zengyongli");
+		people.setUpdateBy("zengyongli");
+		peopleService.deletePeople(people);
+	}
+
+	@Test
+	public void testDeletePeople() throws BizfwServiceException {
+		People people = peopleService.getByCode("zengyongli");
+		people.setUpdateBy("testdelete");
+		peopleService.deletePeople(people);
+
+		People dbPeople = peopleService.getByCode("zengyongli");
+		assertEquals(dbPeople, null);
+	}
+
+	@Test
+	public void testGetPeopleListByDepartment() throws BizfwServiceException {
+		List<People> list = peopleService.getPeopleListByDepartment("8a808087583fa7b701583faadf300000");
+		assertEquals(list.size(), 3);
+	}
+
+	@Test
+	public void testUnlockFail() throws BizfwServiceException {
+		expectedException.expect(BizfwServiceException.class);
+		expectedException.expectMessage(ErrorCode.PeopleDept.People.UNLOCK_FAIL_STATUS_ERR);
+
+		People people = peopleService.getByCode("admin");
+		peopleService.unlock(people);
+	}
+
+	@Test
+	public void testUnlock() throws BizfwServiceException {
+		People people = peopleService.getByCode("1232");
+		peopleService.unlock(people);
+
+		assertEquals(people.getStatus(), People.STATUS_NORMAL);
+	}
+
+	@Test
+	public void testModifyPasswordFail() throws BizfwServiceException {
+		expectedException.expect(BizfwServiceException.class);
+		expectedException.expectMessage(ErrorCode.PeopleDept.People.UPDATE_FAIL_PWD_ERR);
+
+		People people = peopleService.getByCode("admin");
+		peopleService.modifyPassword(people, "666666", "777777");
+	}
+
+	@Test
+	public void testModifyPassword() throws BizfwServiceException {
+		String md5 = StringUtils.encryptMD5("777777");
+		People people = peopleService.getByCode("admin");
+		peopleService.modifyPassword(people, "888888", "777777");
+
+		People dbPeople = peopleService.getByCode("admin");
+		assertEquals(md5, dbPeople.getPassword());
+	}
+}
