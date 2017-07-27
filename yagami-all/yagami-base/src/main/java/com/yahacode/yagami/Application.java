39554674ccf4c9813b7b75479411357844d38531
zeoly <zeoly100@163.com>
2017-07-27 14:22:04 +0800

    add transaction

--- yagami-all/yagami-base/src/main/java/com/yahacode/yagami/Application.java
+++ yagami-all/yagami-base/src/main/java/com/yahacode/yagami/Application.java
@@ -1,5 +1,8 @@
 package com.yahacode.yagami;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,26 +10,33 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
-
-import com.yahacode.yagami.base.impl.BaseDaoImpl;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @EnableEurekaClient
 @ComponentScan(basePackages = "com.yahacode.yagami")
 @EnableAutoConfiguration
 @EntityScan(basePackages = "com.yahacode.yagami")
-@EnableJpaRepositories(repositoryBaseClass = BaseDaoImpl.class)
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean(name = "txManager")
+	public HibernateTransactionManager txManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+		return txManager;
+	}
+
 	@Bean(name = "sessionFactory")
-	public HibernateJpaSessionFactoryBean sessionFactory() {
-		return new HibernateJpaSessionFactoryBean();
+	public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+		HibernateJpaSessionFactoryBean sessionFactory = new HibernateJpaSessionFactoryBean();
+		return sessionFactory;
 	}
 
 }
