ebfc553fc7ae05dabff9a98a06293b51e51f742a
zeoly <zeoly100@163.com>
2017-09-26 14:04:58 +0800

    add SSL

--- yagami-all/yagami-base/src/main/java/com/yahacode/yagami/auth/action/RoleAction.java
+++ yagami-all/yagami-base/src/main/java/com/yahacode/yagami/auth/action/RoleAction.java
@@ -25,42 +25,42 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/role")
 public class RoleAction extends BaseAction {
 
-	@Autowired
-	private RoleService roleService;
+    @Autowired
+    private RoleService roleService;
 
-	@ApiOperation(value = "获取所有角色列表")
-	@ResponseBody
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Role> getRoleList() throws BizfwServiceException {
-		return roleService.getAllRoleList();
-	}
+    @ApiOperation(value = "获取所有角色列表")
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Role> getRoleList() throws BizfwServiceException {
+        return roleService.getAllRoleList();
+    }
 
-	@ApiOperation(value = "新增角色")
-	@ApiImplicitParam(name = "role", value = "角色模型", required = true, dataTypeClass = Role.class)
-	@ResponseBody
-	@RequestMapping(method = RequestMethod.POST)
-	public void addRole(HttpServletRequest request, @RequestBody Role role) throws BizfwServiceException {
-		People people = getLoginPeople(request);
-		role.init(people.getCode());
-		roleService.addRole(role);
-	}
+    @ApiOperation(value = "新增角色")
+    @ApiImplicitParam(name = "role", value = "角色模型", required = true, dataTypeClass = Role.class)
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public void addRole(HttpServletRequest request, @RequestBody Role role) throws BizfwServiceException {
+        People people = getLoginPeople(request);
+        role.init(people.getCode());
+        roleService.addRole(role);
+    }
 
-	@ApiOperation(value = "更新角色信息")
-	@ApiImplicitParam(name = "role", value = "角色模型", required = true, dataTypeClass = Role.class)
-	@ResponseBody
-	@RequestMapping(method = RequestMethod.PATCH)
-	public void modifyRole(HttpServletRequest request, @RequestBody Role role) throws BizfwServiceException {
-		People people = getLoginPeople(request);
-		role.update(people.getCode());
-		roleService.modify(role);
-	}
+    @ApiOperation(value = "更新角色信息")
+    @ApiImplicitParam(name = "role", value = "角色模型", required = true, dataTypeClass = Role.class)
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.PATCH)
+    public void modifyRole(HttpServletRequest request, @RequestBody Role role) throws BizfwServiceException {
+        People people = getLoginPeople(request);
+        role.update(people.getCode());
+        roleService.modify(role);
+    }
 
-	@ApiOperation(value = "删除角色")
-	@ApiImplicitParam(name = "id", value = "角色id", required = true, dataTypeClass = String.class)
-	@ResponseBody
-	@RequestMapping(method = RequestMethod.DELETE, value = "{id}")
-	public void deleteRole(@PathVariable("id") String roleId) throws BizfwServiceException {
-		roleService.deleteRole(roleId);
-	}
+    @ApiOperation(value = "删除角色")
+    @ApiImplicitParam(name = "id", value = "角色id", required = true, dataTypeClass = String.class)
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.DELETE, value = "{id}")
+    public void deleteRole(@PathVariable("id") String roleId) throws BizfwServiceException {
+        roleService.deleteRole(roleId);
+    }
 
 }
