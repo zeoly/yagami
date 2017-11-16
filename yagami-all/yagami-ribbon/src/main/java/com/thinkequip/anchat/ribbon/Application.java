54b6d4c2bc24833869b1d4f60453ae1eb1da84e5
zeoly <zeoly100@163.com>
2017-11-16 18:01:33 +0800

    test hystrix

--- yagami-all/yagami-ribbon/src/main/java/com/thinkequip/anchat/ribbon/Application.java
+++ yagami-all/yagami-ribbon/src/main/java/com/thinkequip/anchat/ribbon/Application.java
@@ -2,6 +2,7 @@ package com.thinkequip.anchat.ribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -9,15 +10,16 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	@LoadBalanced
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
