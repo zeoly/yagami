5ae84450f5b7954a9b1220e5129316c9c93facec
zeoly <zeoly100@163.com>
2018-01-10 17:55:07 +0800

    add bootstrap and facade

--- yagami-all/yagami-app-bootstrap/src/main/java/com/yahacode/yagami/Application.java
+++ yagami-all/yagami-app-bootstrap/src/main/java/com/yahacode/yagami/Application.java
@@ -1,12 +1,11 @@
 package com.yahacode.yagami;
 
-import com.yahacode.yagami.base.filter.LoginFilter;
+import com.yahacode.yagami.config.LoginFilter;
 import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
