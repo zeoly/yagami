b5a77c150c2baf437bdb3d2ad0cb87d9d4815f23
zeoly <zeoly100@163.com>
2018-02-07 17:53:05 +0800

    servlet context

--- yagami/yagami-app-bootstrap/src/main/java/com/yahacode/yagami/config/LoginFilter.java
+++ yagami/yagami-app-bootstrap/src/main/java/com/yahacode/yagami/config/LoginFilter.java
@@ -16,35 +16,35 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.yahacode.yagami.base.BaseAction;
+import com.yahacode.yagami.base.consts.SessionKeyConsts;
 import com.yahacode.yagami.pd.model.People;
 
 public class LoginFilter implements Filter {
 
-	List<String> excludedUrls;
+    List<String> excludedUrls;
 
-	@Override
-	public void init(FilterConfig config) throws ServletException {
+    @Override
+    public void init(FilterConfig config) throws ServletException {
 
-	}
+    }
 
-	@Override
-	public void destroy() {
+    @Override
+    public void destroy() {
 
-	}
+    }
 
-	@Override
-	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-			throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) servletRequest;
-		HttpServletResponse response = (HttpServletResponse) servletResponse;
-		String url = request.getRequestURI();
-		if (!isExcluded(url)) {
-			HttpSession session = request.getSession();
-			People mockPeople = new People("test");
-			mockPeople.setCode("test");
-			mockPeople.setIdBfPeople("8a808088579f5d5501579f5d99790000");
-			mockPeople.setDepartmentId("8a808087583fa7b701583faadf300000");
-			session.setAttribute(BaseAction.PEOPLE_KEY, mockPeople);
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String url = request.getRequestURI();
+        if (!isExcluded(url)) {
+            HttpSession session = request.getSession();
+            People mockPeople = new People("test");
+            mockPeople.setCode("test");
+            mockPeople.setIdBfPeople("8a808088579f5d5501579f5d99790000");
+            mockPeople.setDepartmentId("8a808087583fa7b701583faadf300000");
+            session.setAttribute(SessionKeyConsts.LOGIN_PEOPLE, mockPeople);
 //			People peopleInfo = (People) session.getAttribute(BaseAction.PEOPLE_KEY);
 //			if (peopleInfo == null) {
 //				response.setCharacterEncoding("UTF-8");
@@ -54,22 +54,22 @@ public class LoginFilter implements Filter {
 //				response.getWriter().write(mapper.writeValueAsString(yagamiResponse));
 //				return;
 //			}
-		}
-		filterChain.doFilter(servletRequest, servletResponse);
-	}
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
 
-	private boolean isExcluded(String url) {
-		for (String str : excludedUrls) {
-			Pattern pattern = Pattern.compile(str);
-			Matcher matcher = pattern.matcher(url);
-			if (matcher.find()) {
-				return true;
-			}
-		}
-		return false;
-	}
+    private boolean isExcluded(String url) {
+        for (String str : excludedUrls) {
+            Pattern pattern = Pattern.compile(str);
+            Matcher matcher = pattern.matcher(url);
+            if (matcher.find()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public void setExcludedUrls(List<String> excludedUrls) {
-		this.excludedUrls = excludedUrls;
-	}
+    public void setExcludedUrls(List<String> excludedUrls) {
+        this.excludedUrls = excludedUrls;
+    }
 }
