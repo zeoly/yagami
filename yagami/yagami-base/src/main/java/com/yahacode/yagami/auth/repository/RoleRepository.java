94692b452f1ae7af9eda6f7fb9bc3f4c40e3da5c
zeoly <zeoly100@163.com>
2018-03-28 15:04:24 +0800

    add jpa repo

---
+++ yagami/yagami-base/src/main/java/com/yahacode/yagami/auth/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.yahacode.yagami.auth.repository;
+
+import com.yahacode.yagami.auth.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author zengyongli 2018-03-27
+ */
+public interface RoleRepository extends JpaRepository<Role, String> {
+
+}
---
+++ yagami/yagami-base/src/main/java/com/yahacode/yagami/pd/repository/PeopleRepository.java
@@ -0,0 +1,12 @@
+package com.yahacode.yagami.pd.repository;
+
+import com.yahacode.yagami.pd.model.People;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author zengyongli 2018-03-27
+ */
+public interface PeopleRepository extends JpaRepository<People, String> {
+
+
+}
