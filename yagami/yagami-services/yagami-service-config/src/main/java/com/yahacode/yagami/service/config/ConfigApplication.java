a03f0565a6303c4ded962cc6069990e0c220e5ca
zeoly <zeoly100@163.com>
2018-05-25 18:01:11 +0800

    combine config and eureka

--- yagami/yagami-services/yagami-service-config/src/main/java/com/yahacode/yagami/service/config/ConfigApplication.java
+++ yagami/yagami-services/yagami-service-config/src/main/java/com/yahacode/yagami/service/config/ConfigApplication.java
@@ -2,11 +2,13 @@ package com.yahacode.yagami.service.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author zengyongli 2018-05-23
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
