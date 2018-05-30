9c8a02eba5fbca756c0a4dbd076cfd8c7bb8f1b1
zeoly <zeoly100@163.com>
2018-05-30 17:39:55 +0800

    add zipkin server

---
+++ yagami/yagami-services/yagami-service-zipkin/src/main/java/com/yahacode/yagami/service/zipkin/ZipkinApplication.java
@@ -0,0 +1,17 @@
+package com.yahacode.yagami.service.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * @author zengyongli 2018-05-30
+ */
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
