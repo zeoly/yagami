56e1557c651dbcc2a6b99e1cd3ad0954c377965f
zeoly <zeoly100@163.com>
2018-06-05 16:38:36 +0800

    update model

--- yagami/yagami-modules/yagami-base/src/main/java/com/yahacode/yagami/geo/model/Division.java
+++ yagami/yagami-modules/yagami-base/src/main/java/com/yahacode/yagami/geo/model/Division.java
@@ -1,16 +1,36 @@
 package com.yahacode.yagami.geo.model;
 
 import com.yahacode.yagami.base.BaseModel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * @author zengyongli 2018-05-16
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bf_division")
 public class Division extends BaseModel {
 
+    /**
+     * primary key
+     */
+    @Id
+    @Column(name = "id_bf_division")
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "uuid")
     private String idBfDivision;
 
     private String name;
